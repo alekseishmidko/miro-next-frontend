a58956cb0ff7780aa109b8190970bf977a14289f
alekseishmidko <alexeishmidko@gmail.com>
2025-05-18 23:52:16 +0400

    access reftesh tokens

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -2,8 +2,8 @@ import { ROUTES } from "../shared/model/routes";
 import { createBrowserRouter, redirect } from "react-router-dom";
 import { App } from "./app";
 import { Providers } from "./providers";
-// import { protectedLoader, ProtectedRoute } from "./protected-route";
-// import { AppHeader } from "@/features/header";
+import { protectedLoader, ProtectedRoute } from "./protected-route";
+import { AppHeader } from "@/features/header";
 
 export const router = createBrowserRouter([
   {
@@ -13,35 +13,35 @@ export const router = createBrowserRouter([
       </Providers>
     ),
     children: [
-      //   {
-      //     loader: protectedLoader,
-      //     element: (
-      //       <>
-      //         <AppHeader />
-      //         <ProtectedRoute />
-      //       </>
-      //     ),
-      //     children: [
-      //       {
-      //         path: ROUTES.BOARDS,
-      //         lazy: () => import("@/features/boards-list/boards-list.page"),
-      //       },
-      //       {
-      //         path: ROUTES.FAVORITE_BOARDS,
-      //         lazy: () =>
-      //           import("@/features/boards-list/boards-list-favorite.page"),
-      //       },
-      //       {
-      //         path: ROUTES.RECENT_BOARDS,
-      //         lazy: () =>
-      //           import("@/features/boards-list/boards-list-recent.page"),
-      //       },
-      //       {
-      //         path: ROUTES.BOARD,
-      //         lazy: () => import("@/features/board/board.page"),
-      //       },
-      //     ],
-      //   },
+      {
+        loader: protectedLoader,
+        element: (
+          <>
+            <AppHeader />
+            <ProtectedRoute />
+          </>
+        ),
+        children: [
+          {
+            path: ROUTES.BOARDS,
+            lazy: () => import("@/features/boards-list/boards-list.page"),
+          },
+          // {
+          //   path: ROUTES.FAVORITE_BOARDS,
+          //   lazy: () =>
+          //     import("@/features/boards-list/boards-list-favorite.page"),
+          // },
+          // {
+          //   path: ROUTES.RECENT_BOARDS,
+          //   lazy: () =>
+          //     import("@/features/boards-list/boards-list-recent.page"),
+          // },
+          {
+            path: ROUTES.BOARD,
+            lazy: () => import("@/features/board/board.page"),
+          },
+        ],
+      },
 
       {
         path: ROUTES.LOGIN,
